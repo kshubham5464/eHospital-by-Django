6f2d17970ee8496bebc9c160fc8333c7fbdd9fe2
kshubham5464 <kshubham5464@gmail.com>
2025-11-24 13:57:49 +0530

    Add STATIC_ROOT and whitenoise config for Render

--- eHospital/settings.py
+++ eHospital/settings.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 # ===========================
 # Security / Debug Settings
